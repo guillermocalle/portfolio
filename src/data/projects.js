aa8372d7d0a8212aa8d24e4116d85943ee113ddb
Guillermo Calle <guillermocalle011@gmail.com>
2023-12-13 06:47:18 -0400

    add restaurant project

--- src/data/projects.js
+++ src/data/projects.js
@@ -1,27 +1,35 @@
-import project1 from '../assets/projects/remotekonnect.png';
-import project2 from '../assets/projects/digiprocp.png';
-import project3 from '../assets/projects/tiendaivet.png';
+import project1 from "../assets/projects/remotekonnect.png";
+import project2 from "../assets/projects/digiprocp.png";
+import project3 from "../assets/projects/tiendaivet.png";
+import project4 from "../assets/projects/restaurantedonalizbeth.png";
 
 export default [
   {
-    title: 'Tienda Ivet',
+    title: "Tienda Ivet",
     image: project3,
-    tech: ['HTML', 'ReactJS', 'TailwindCSS'],
-    linkdemo: 'https://tiendaivet.vercel.app/', 
-    linkgithub: 'https://github.com/guillermocalle/ecommerce-web',
+    tech: ["HTML", "ReactJS", "TailwindCSS"],
+    linkdemo: "https://tiendaivet.vercel.app/",
+    linkgithub: "https://github.com/guillermocalle/ecommerce-web",
   },
   {
-    title: 'DigiProCP',
+    title: "DigiProCP",
     image: project2,
-    tech: ['HTML', 'ReactJS', 'TailwinCSS'],
-    linkdemo: 'https://digiprocp.vercel.app/', 
-    linkgithub: 'https://github.com/guillermocalle/digiprocp-web',
+    tech: ["HTML", "ReactJS", "TailwinCSS"],
+    linkdemo: "https://digiprocp.vercel.app/",
+    linkgithub: "https://github.com/guillermocalle/digiprocp-web",
   },
   {
-    title: 'RemoteKonnect',
+    title: "RemoteKonnect",
     image: project1,
-    tech: ['HTML', 'ReactJS', 'TailwinCSS'],
-    linkdemo: 'https://remotekonnect.vercel.app/', 
-    linkgithub: 'https://github.com/guillermocalle/course-academy-website',
+    tech: ["HTML", "ReactJS", "TailwinCSS"],
+    linkdemo: "https://remotekonnect.vercel.app/",
+    linkgithub: "https://github.com/guillermocalle/course-academy-website",
+  },
+  {
+    title: "Restaurante Dona Lizbeth",
+    image: project4,
+    tech: ["HTML", "ReactJS", "TailwinCSS"],
+    linkdemo: "https://restaurantedonalizbeth.vercel.app/",
+    linkgithub: "https://github.com/guillermocalle/restaurant-web",
   },
 ];
