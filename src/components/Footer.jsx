21373e6b4374ecdfc979397f1796bc283d230360
Guillermo Calle <guillermocalle011@gmail.com>
2023-12-16 18:32:44 -0400

    modified details

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,15 +1,22 @@
-import React from 'react'
+import React from "react";
 
 function Footer() {
   return (
-    <div className='py-5 bg-slate-800 text-center text-gray-300 rounded-t-lg'>
-      <a href="#hero" className='block text-xl md:text-2xl font-semibold'>Guillermo Calle</a>
-      <a href="mailto:guillermocalle011@gmail.com" className='text-sm md:text-md hover:text-indigo-500'>guillermocalle011@gmail.com</a>
-      <p className='text-xs mt-2 text-gray-500'>
-       © guillermo.cp {new Date().getFullYear()}. All rights reserved
+    <div className="py-5 bg-slate-800 text-center text-gray-300 rounded-t-lg">
+      <a href="#hero" className="block text-xl md:text-2xl font-semibold">
+        Guillermo Calle
+      </a>
+      <a
+        href="mailto:guillermo.calle.info@gmail.com"
+        className="text-sm md:text-md hover:text-indigo-500"
+      >
+        guillermo.calle.info@gmail.com
+      </a>
+      <p className="text-xs mt-2 text-gray-500">
+        © guillermo.cp {new Date().getFullYear()}. All rights reserved
       </p>
     </div>
-  )
+  );
 }
 
-export default Footer
+export default Footer;
